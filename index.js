0c96ec67a1dda7114a2f21ec39e743749154b207
Chandan Haldar <105099116+chandanH001@users.noreply.github.com>
2025-05-20 13:27:42 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,80 @@
+/**
+ * var x = 59;
+     x+=20;
+     console.log(x);
+ * 
+ */
+
+// Positive , negative, or zero solutions 
+/*
+let number = -4;
+if(number > 0){
+console.log('this number is Posative');
+
+} else if (number < 0){
+    console.log('This number is Nagative'); 
+}else {
+    console.log('Opps Its Zero');
+}
+
+
+// Even And Odd Number 
+let number = 25 ;
+if (number % 2 ==0){
+    console.log('This is Even Number'); 
+}
+else {
+    console.log('Its Odd Number');
+}
+    
+
+const burger = 300;
+let total_price = burger+30;
+if (burger > 500 ){
+
+    console.log('You Got Free Cake')
+
+}else {
+    console.log('Cake price is ' + 30 +  ' Total price is ' + total_price);
+}
+
+
+// Array .........................
+
+const number = [4,5,6,8,9,2,0,3,2];
+
+console.log(number.length);
+
+// last array element add
+number.push(10);
+console.log(number);
+
+//last element remove
+number.pop();
+console.log(number);
+
+// first element add
+number.unshift(30);
+console.log(number);
+
+
+//first element remove
+number.shift();
+console.log(number);
+
+// includes use case for find element from array
+
+console.log (number.includes(80));
+
+*/
+
+// For of loop 
+
+const fruits = ['apple','orange', 'banana', 'jackfood'];
+
+for(const fruit of fruits){
+    console.log(fruit + ' Ami Khabo');
+}
+
+
+
