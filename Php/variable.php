b5afb8bf36f7547b717d999a02c15d5d5c3bbff1
Chandan <chandan.halder96@gmail.com>
2025-07-28 19:41:33 +0600

    Your meaningful commit message

---
+++ Php/variable.php
@@ -0,0 +1,15 @@
+<?php
+
+$name = "chandan haldar \n";
+echo $name;
+
+// constant variable
+define('PI', 3.14156);
+
+echo "the value of PI =" . PI . "\n";
+// printf example
+
+$city    = 'Dhaka';
+$country = "Bangladesh";
+
+printf("i live in %s of %s", $city, $country);
